f85c379a60d9c0d2a27238f11bb3fd8c7bb8d934
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-04-25 12:43:43 +0200

    Update create_themes.py

--- create_themes.py
+++ create_themes.py
@@ -8,13 +8,9 @@ with open('config.yml', 'r') as file:
     mongo_url = safe_load(file)['mongo_url']
 client = MongoClient(mongo_url)
 db = client.aerodb
-driver = webdriver.Chrome()
-
-scrapper = Scrapper(driver)
-print(scrapper.get_themes_mappers())
 
 
-"""def get_ids(collection):
+def get_ids(collection):
     items = list(collection.find({}, {'_id': True}))
     return [str(item['_id']) for item in items]
 
@@ -22,12 +18,27 @@ print(scrapper.get_themes_mappers())
 categories = get_ids(db['Categories'])
 themes = get_ids(db['Themes'])
 
+db['QCMs'].update_many({}, {"$set": {"free": False}})
+
 for category in categories:
     for theme in themes:
         find = {'category': category, 'theme': theme}
 
-        total = db['QCMs'].count_documents({'theme': theme})
+        total = db['QCMs'].count_documents({'theme': theme, 'category':category})
         free = total // 10
 
-        print(themes)
-"""
+        if free < 10:
+            free = total
+
+        to_update = list(db['QCMs'].find(find, {'_id'}).limit(free))
+
+        if to_update:
+            to_update = [qcm['_id'] for qcm in to_update]
+
+            for _id in to_update:
+                db['QCMs'].update_one({"_id": _id}, {"$set": {'free': True}})
+            
+
+
+
+
